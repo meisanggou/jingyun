d1bd380298d6df7f53cf6bfff9a77a745fed99ad
local10 <local10@gene.ac>
2018-08-02 17:23:58 +0800

    0.4.5

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ if sys.version_info <= (2, 7):
     sys.exit(1)
 
 name = "jingyun-cli"
-version = "0.4.4"
+version = "0.4.5"
 url = "https://github.com/meisanggou/jingyun"
 license = "MIT"
 author = "meisanggou"
